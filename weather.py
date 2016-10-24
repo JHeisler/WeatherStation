f026c9ebcd194545c58ffeccca717cc6edccfec8
jzbass25 <jzbass25@yahoo.com>
2016-10-23 21:35:49 -0400

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,40 @@
+#import libraries
+import sys
+import Adafruit_DHT # humidity
+# BMP180/085
+import Adafruit_BMP.BMP085 as BMP085
+import gevent # websocket
+import datetime
+import socket
+
+# define sensors and pins
+sensorDHT = Adafruit_DHT.AM2302
+pin = 23
+sensorBMP = BMP.BMP085()
+
+
+s = socket.socket()
+host = socket.gethostname()
+port = 80
+
+s.connect((host,port))
+
+s.recv(1024)
+
+def msg():
+
+    while 1:
+        humidity = Adafruit_DHT.read_retry(sensorDHT,pin)
+        hum = "{:0.1f}%".format(humidity)
+        temp = "{0:0.2f} *C".format(sensorBMP.read_temperature())
+        Pressure = "{0:0.2f} Pa".format(sensorBMP.read_pressure())
+        t = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        s.send(hum)
+        s.send(temp)
+        s.send(Pressure)
+        s.send(t)
+        print "sent"
+
+
+    s.shutdown(0)
+    s.close
