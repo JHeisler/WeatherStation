cafd7a8a4bfde87ed4150a0d2311fdbe9ff3947a
jzbass25 <jzbass25@yahoo.com>
2016-11-04 19:29:43 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -19,20 +19,18 @@ s = SocketIO('localhost/Store', 8080)
 s.wait(seconds=1)
 
 
-def data(self):
-
-    while 1:
-        humidity = Adafruit_DHT.read_retry(sensorDHT,pin)
-        values[0] = "{:0.1f}%".format(humidity)
-        values[1] = "{0:0.2f} *C".format(sensorBMP.read_temperature())
-        values[2] = "{0:0.2f} Pa".format(sensorBMP.read_pressure())
-        values[3] = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-        print values[3]
-        #serialize data to send
-        data = pickle.dump(values)
-        s.emit('data',data)
-        print "sent"
+while 1:
+    humidity = Adafruit_DHT.read_retry(sensorDHT,pin)
+    values[0] = "{:0.1f}%".format(humidity)
+    values[1] = "{0:0.2f} *C".format(sensorBMP.read_temperature())
+    values[2] = "{0:0.2f} Pa".format(sensorBMP.read_pressure())
+    values[3] = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    print values[3]
+    #serialize data to send
+    data = pickle.dump(values)
+    s.emit('data',data)
+    print "sent"
 
 
-    s.shutdown(0)
-    s.close
+s.shutdown(0)
+s.close
