768fd1f4c80cfc527a0fd2e1c7fdda5affb612c4
jzbass25 <jzbass25@yahoo.com>
2016-11-05 11:40:08 -0400

    Add files via upload

---
+++ server2.py
@@ -0,0 +1,58 @@
+import sys
+import SocketServer
+import pickle
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
+
+data2 = [0,0,0,0]
+
+def insert_data(data): 
+    query = "INSERT INTO Weather(Humidity,Temperature,Pressure,DTime) " \
+            "VALUES(%s,%s,%s,%s)"
+    args = (data[0], data[1],data[2],data[3])
+
+    try:
+        db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+
+        if cursor.lastrowid:
+            print('last insert id', cursor.lastrowid)
+        else:
+            print('last insert id not found')
+
+        conn.commit()
+    except Error as error:
+        print(error)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+def request_handler(request_socket):
+    try:
+        request_msg = request_socket.recv(1024)
+        data2 = pickle.load(request_msg)
+        insert_data(data2)
+        request_socket.send('HTTP/1.0 200 Ok')
+
+    except Exception, ex:
+        print 'e', ex,
+
+def simple_tcp_server():
+    tcp_server = SocketServer.TCPServer(("localhost", 8080),
+                                        RequestHandlerClass=None,
+                                        bind_and_activate=True)
+
+    while True:
+        request_socket, address_port_tuple = tcp_server.get_request()
+        print "Connection from: %s" % str(address_port_tuple)
+
+        request_handler(request_socket)
+        # shutdown request socket and close it
+        tcp_server.shutdown_request(request_socket)
+
+if __name__ == "__main__":
+    simple_tcp_server()
