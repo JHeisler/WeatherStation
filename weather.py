880e28b1836c084e6047da858fd53569dc775596
jzbass25 <jzbass25@yahoo.com>
2016-11-06 13:24:29 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,16 +1,16 @@
 #import libraries
 from gevent import monkey; monkey.patch_all()
 import sys
-import Adafruit_DHT # humidity
+import Adafruit_DHT
 #BMP180/085
 import Adafruit_BMP.BMP085 as BMP085
 import datetime
 from socketIO_client import SocketIO
-import pickle
-import time
+import json
 import logging
 
 logging.basicConfig()
+
 # define sensors and pins
 sensorDHT = Adafruit_DHT.DHT22
 pin = 26
@@ -18,18 +18,18 @@ sensorBMP = BMP085.BMP085()
 
 values = [0,0,0,0]
 
-s = SocketIO('localhost/', 8080)
+s = SocketIO('localhost:8080')
 
 while 1:
-    values[0] = Adafruit_DHT.read_retry(sensorDHT,pin)
-    values[1] = sensorBMP.read_temperature()
-    values[2] = sensorBMP.read_pressure()
-    values[3] = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    print values[0]
-    #serialize data to send
-    data = pickle.dump(values)
-    s.emit('data',data)
-    print "sent"
+	dht = Adafruit_DHT.read_retry(sensorDHT,pin)
+	values[0] = dht[0]
+	values[1] = sensorBMP.read_temperature()
+	values[2] = sensorBMP.read_pressure()
+	values[3] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	#serialize data to send
+	data = json.dumps(values)
+	s.emit('data',data)
+	print "sent: " + data
 
 s.shutdown(0)
 s.close
